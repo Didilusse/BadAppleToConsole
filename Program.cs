ec422300cf4dbbd3d08c138e98061890fbb495f5
Adil Rahmani <didilusse@gmail.com>
2025-02-14 10:44:49 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using OpenCvSharp;
+
+class Program
+{
+    static void Main()
+    {
+        string videoFile = "video.mp4";
+        System.IO.Directory.CreateDirectory("frames");
+
+        using var capture = new VideoCapture(videoFile);
+        using var window = new Window("Frame Preview");
+        Mat frame = new Mat();
+
+        int frameNumber = 0;
+        
+    }
+}
